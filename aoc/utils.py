afa0ba2ab9a09e035088fc887a31c5855d635403
Teddy Tran <teddy.tran@freenome.com>
2022-12-08 18:35:58 -0800

    updated

--- aoc/utils.py
+++ aoc/utils.py
@@ -4,3 +4,6 @@ def parse_file(filepath: str) -> Sequence[str]:
     with open(filepath) as file:
         data = file.read()
         return data.split("\n")[:-1] # ignore the newline at the end
+
+def parse_string(data: str) -> Sequence[str]:
+    return data.split("\n")
