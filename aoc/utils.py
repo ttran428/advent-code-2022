76d09e4e5b617d4ed12cd48097a3962170a8aaf4
Teddy Tran <teddy.tran@freenome.com>
2022-12-12 13:56:58 -0800

    updated

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,14 +1,14 @@
-from typing import Sequence
+from typing import List
 
-def parse_file(filepath: str) -> Sequence[str]:
+def parse_file(filepath: str) -> List[str]:
     with open(filepath) as file:
         data = file.read()
         return data.split("\n")[:-1] # ignore the newline at the end
 
-def parse_string(data: str) -> Sequence[str]:
+def parse_string(data: str) -> List[str]:
     return data.split("\n")
 
-def parse_by_split_lines(filepath: str, string: bool = False) -> Sequence[Sequence[str]]:
+def parse_by_split_lines(filepath: str, string: bool = False) -> List[List[str]]:
     if string:
         data = parse_string(filepath)
     else:
